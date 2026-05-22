322dde9789f254075cf166178f314826153f72ca
vladsid426-cpu <vladsid426@gmail.com>
2026-05-22 14:36:44 +0000

    commit all

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Booking,Room,RoomCategory
+
+# Create your views here.
+def room_list(requests):
+    room_list = Room.objects.all()
+
+    return render(requests,'room_list.html',{'room_list':room_list})
