1f8cd4b060f30e74666c327d42e5fe28477d2599
vladsid426-cpu <vladsid426@gmail.com>
2026-06-19 18:33:55 +0000

    Commit & Push

--- booking_system/auth/models.py
+++ booking_system/auth/models.py
@@ -1,26 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User, AbstractUser  
 
-
-class UserRole(models.TextChoices):
-    USER = "user", _("юзер")
-    MODERATOR = "moderator", _("модер")
-    ADMIN = "admin", _("адмін")
-
+class User_Role(models.TextChoices):
+    user = models.CharField(max_length=15, choices=[("user", "юзер")])
+    moderator = models.CharField(max_length=15, choices=[("moderator", "модер")])
+    admin = models.CharField(max_length=15, choices=[("admin", "адмін")])
 
 class User(AbstractUser):
-    role = models.CharField(
-        max_length=15,
-        choices=UserRole.choices,
-        default=UserRole.USER.value,
-        verbose_name=_("Роль"),
-    )
-    bio = models.TextField(blank=True, verbose_name=_("Біографія"))
+        
+        role = models.ForeignKey(User_Role, on_delete=models.CASCADE)
+        bio = models.TextField()
 
-    class Meta:
-        verbose_name = _("Користувач")
-        verbose_name_plural = _("Користувачі")
-
-    def __str__(self):
-        return self.username
