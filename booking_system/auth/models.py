3e3006a26a3dcbb06b71be0cc4ae5fd041889537
vladsid426-cpu <vladsid426@gmail.com>
2026-06-19 18:44:53 +0000

    Comiit & Push

--- booking_system/auth/models.py
+++ booking_system/auth/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser  
 
 class User_Role(models.TextChoices):
-    user = models.CharField(max_length=15, choices=[("user", "юзер")])
-    moderator = models.CharField(max_length=15, choices=[("moderator", "модер")])
-    admin = models.CharField(max_length=15, choices=[("admin", "адмін")])
+    USER = "user", "юзер"
+    MODERATOR = "moderator", "модер"
+    ADMIN = "admin", "адмін"\
 
 class User(AbstractUser):
         
         role = models.ForeignKey(User_Role, on_delete=models.CASCADE)
         bio = models.TextField()
-
